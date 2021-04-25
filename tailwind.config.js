bf562951bf7682ac8ccbf78767fb20599a2bf55e
Hang Nguyen <hangnguyen@Hangs-MacBook-Air.local>
2021-04-26 03:27:03 +0700

    create a form and change tailwind configs

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  future: {
+    removeDeprecatedGapUtilities: true
+  },
+  theme: {
+    fill: (theme) => ({
+      red: theme('colors.red.primary')
+    }),
+    colors: {
+      white: '#ffffff',
+      pink: {
+        medium: '#d94f70'
+      },
+      black: {
+        light: '#005c98',
+        faded: '#00000059'
+      },
+      gray: {
+        base: '#616161',
+        background: '#fafafa',
+        primary: '#dbdbdb'
+      },
+      red: {
+        primary: '#ed4956'
+      }
+    }
+  }
+};
