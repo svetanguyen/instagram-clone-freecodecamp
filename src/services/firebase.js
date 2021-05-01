49fea5dfae42fd629d17345872b50a3f490c27a0
svetanguyen <matematicaforeber@gmail.com>
2021-05-02 00:06:51 +0700

    fix misspelling

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -80,7 +80,7 @@ export async function getPhotos(userId, following) {
     .where('userId', 'in', following)
     .get();
 
-  const userFollowedPhotos = result.doc.map((photo) => ({
+  const userFollowedPhotos = result.docs.map((photo) => ({
     ...photo.data(),
     docId: photo.id
   }));
