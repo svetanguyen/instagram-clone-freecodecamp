fb4fdb9ebba7ed6a416a4a5ca3ab3e447861f9a2
svetanguyen <matematicaforeber@gmail.com>
2021-05-01 22:55:53 +0700

    get an array of photos to the timeline

---
+++ src/hooks/use-photos.js
@@ -0,0 +1,28 @@
+/* eslint-disable no-unused-vars */
+import { useState, useEffect, useContext } from 'react';
+import UserContext from '../context/user';
+import { getUserByUserId, getPhotos } from '../services/firebase';
+
+export default function usePhotos() {
+  const [photos, setPhotos] = useState(null);
+  const {
+    user: { uid: userId = '' }
+  } = useContext(UserContext);
+
+  useEffect(() => {
+    async function getTimeLinePhotos() {
+      const [{ following }] = await getUserByUserId(userId);
+      let followedUsersPhotos = [];
+      if (following.length > 0) {
+        followedUsersPhotos = await getPhotos(userId, following);
+      }
+
+      followedUsersPhotos.sort((a, b) => b.dateCreated - a.dateCreated);
+      setPhotos(followedUsersPhotos);
+    }
+    getTimeLinePhotos();
+    console.log(userId);
+  }, [userId]);
+
+  return { photos };
+}
