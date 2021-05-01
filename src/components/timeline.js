4d4c7dd1130f55e569c9552f6ab6f5a4b65185c3
svetanguyen <matematicaforeber@gmail.com>
2021-05-02 00:07:19 +0700

    show posts on timeline:

--- src/components/timeline.js
+++ src/components/timeline.js
@@ -1,9 +1,26 @@
+/* eslint-disable array-callback-return */
+/* eslint-disable react/no-array-index-key */
+/* eslint-disable no-nested-ternary */
+/* eslint-disable no-unused-vars */
 import React from 'react';
+import Skeleton from 'react-loading-skeleton';
+import usePhotos from '../hooks/use-photos';
+import Post from './post';
 
 export default function Timeline() {
+  const { photos } = usePhotos();
+  console.log('photos', photos);
   return (
     <div className="container col-span-2">
-      <p>I am the timeline</p>
+      {!photos ? (
+        <>
+          <Skeleton className="mb-5" count={4} width={640} height={500} />;
+        </>
+      ) : photos?.length > 0 ? (
+        photos.map((content) => <Post key={content.docId} content={content} />)
+      ) : (
+        <p className="text-center text-2xl">Follow people to see photos</p>
+      )}
     </div>
   );
 }
