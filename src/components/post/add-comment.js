3a5292c8e1ff15a375154359784c324877f39d58
svetanguyen <matematicaforeber@gmail.com>
2021-05-05 01:22:36 +0700

    create form for comment submission

---
+++ src/components/post/add-comment.js
@@ -0,0 +1,68 @@
+/* eslint-disable jsx-a11y/control-has-associated-label */
+/* eslint-disable react/forbid-prop-types */
+/* eslint-disable no-unused-vars */
+import React, { useState, useContext } from 'react';
+import PropTypes from 'prop-types';
+import FirebaseContext from '../../context/firebase';
+import UserContext from '../../context/user';
+import User from '../sidebar/user';
+
+export default function AddComment({
+  docId,
+  comments,
+  setComments,
+  commentInput
+}) {
+  const [comment, setComment] = useState('');
+  const { firebase, FieldValue } = useContext(FirebaseContext);
+  const {
+    user: { displayName }
+  } = useContext(UserContext);
+
+  const handleSubmitComment = (event) => {
+    event.preventDefault();
+
+    return null;
+  };
+
+  return (
+    <div className="border-t border-gray-primary">
+      <form
+        method="POST"
+        onSubmit={(event) =>
+          comment.length >= 1 ? handleSubmitComment() : event.preventDefault()
+        }
+        className="flex justify-between pl-0 pr-5"
+      >
+        <input
+          aria-label="Add a comment"
+          autoComplete="off"
+          className="text-sm text-gray-base w-full mr-3 py-5 px-4"
+          type="text"
+          name="add-comment"
+          placeholder="Add a comment..."
+          value={comment}
+          onChange={(target) => setComment(target.value)}
+          ref={commentInput}
+        />
+        <button
+          className={`text-sm font-bold text-pink-medium ${
+            !comment && 'opacity-25'
+          }`}
+          type="button"
+          disabled={comment.length < 1}
+          onClick={handleSubmitComment}
+        >
+          Post
+        </button>
+      </form>
+    </div>
+  );
+}
+
+AddComment.propTypes = {
+  docId: PropTypes.string.isRequired,
+  comments: PropTypes.array.isRequired,
+  setComments: PropTypes.func.isRequired,
+  commentInput: PropTypes.object.isRequired
+};
